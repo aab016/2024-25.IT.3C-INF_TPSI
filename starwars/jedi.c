12327297288251f70ccd91aac421ad5b3e322cdc
aab016 <prof.alessandro_aglietti@iislagrange.edu.it>
2025-03-15 18:12:05 +0000

    fix: int main signature

--- starwars/jedi.c
+++ starwars/jedi.c
@@ -1,11 +1,10 @@
 #include <stdio.h>  // printf
-#include <stdlib.h> // exit
 
 #include "tatooine.h"
 
-void main()
+int main()
 {
     int abitanti_di_tatooine = numero_abitanti();
     printf("Gli abitanti di Tatooine sono %d, che la forza sia con te", abitanti_di_tatooine);
-    exit(0);
+    return 0;
 }
